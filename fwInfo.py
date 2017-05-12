80d778c0501a3dd789360d435e9cb37d3e64afd6
william <1053418947@qq.com>
2017-05-12 15:55:28 +0800

    add fw info

---
+++ fwInfo.py
@@ -0,0 +1,15 @@
+username = 'user'
+password = 'password'
+Server1_bmcip = "http://192.168.0.70"
+Server2_bmcip = "http://192.168.0.71"
+Server3_bmcip = "http://192.168.0.72"
+Server4_bmcip = "http://192.168.0.73"
+server5_bmcip = "http://192.168.0.74"
+server6_bmcip = "http://192.168.0.75"
+immImage = "---"
+
+Official_immImage = "---"
+daan_uefiImage = "---"
+xinyi_uefiImage = "---"
+yuanshan_uefiImage = "---"
+dsaImage = "---"
